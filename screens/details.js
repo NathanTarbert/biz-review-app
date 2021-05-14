dd8210c093b581557ccf5a762e1024cec5fdd3d7
Nathan Tarbert <nate4t@hotmail.comm>
2021-05-14 15:15:48 -0700

    pass the navigation props from the home screen to the details screen and render the text with a back button

--- screens/details.js
+++ screens/details.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Button } from 'react-native';
 
 import { globalStyles } from '../styles/global';
 
-export default function Details() {
+export default function Details({ navigation }) {
+
+    // pressHandler = () => {//this gives tells our button to go back by accessing the navigation prop
+    //     navigation.goBack();
+    // };
+
     return (
         <View style={globalStyles.container} >
-            <Text>Details Screen</Text>
+            <Text>{ navigation.getParam('title') }</Text>
+            <Text>{ navigation.getParam('body') }</Text>
+            <Text>{ navigation.getParam('rating') }</Text>
         </View>
     )
 }
