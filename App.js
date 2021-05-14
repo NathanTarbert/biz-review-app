a125aae643ca92e14024f3b410cb244ec8a4bdb5
Nathan Tarbert <nate4t@hotmail.comm>
2021-05-14 14:20:39 -0700

    stack nagivator bones set up

--- App.js
+++ App.js
@@ -1,10 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useState} from 'react';
+import * as Font from 'expo-font';
 import { StyleSheet, Text, View } from 'react-native';
 import Home from './screens/home';
+import AppLoading from 'expo-app-loading';
+import Navigator from './routes/homeStack';
+
+const getFonts = () => Font.loadAsync({
+    'nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
+    'nunito-bold': require('./assets/fonts/Nunito-Bold.ttf')
+  });
+
 
 export default function App() {
-  return (
-    <Home />
-  );
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  if(fontsLoaded){
+    return (
+    <Navigator />
+    );
+  } else {
+    return(
+      <AppLoading 
+        startAsync={getFonts}
+        onFinish={() => setFontsLoaded(true)}
+        onError={console.warn('App is loading, please be patient')}
+      />
+    )
+  }
+  
 }
--- screens/home.js
+++ screens/home.js
@@ -1,17 +1,35 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Button } from 'react-native';
+import { Audio } from 'expo-av';
+
+import { globalStyles } from '../styles/global';
 
 export default function Home() {
+
+    const [sound, setSound] = React.useState();
+
+  async function playSound() {
+    console.log('Loading Sound');
+    const { sound } = await Audio.Sound.createAsync(
+       require('../assets/sounds/mixkit-rising-harp-music-691.wav')
+    );
+    setSound(sound);
+
+    console.log('Playing Sound');
+    await sound.playAsync(); }
+
+  React.useEffect(() => {
+    return sound
+      ? () => {
+          console.log('Unloading Sound');
+          sound.unloadAsync(); }
+      : undefined;
+  }, [sound]);
+
     return (
-        <View style={styles.container} >
-            <Text>Home Screen</Text>
+        <View style={globalStyles.container} >
+            <Text style={globalStyles.titleText}>Home Screen</Text>
+            <Button title="Play Sound" onPress={playSound} />
         </View>
     )
 }
-
-const styles = StyleSheet.create({
-    container: {
-        padding: 24,
-
-    }
-});
